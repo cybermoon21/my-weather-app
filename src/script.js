800a5f118a872ca3c79be8b0304e424980f39a97
kontinew <cybermoon21k@gmail.com>
2025-05-23 05:41:31 +0200

    Updated time and date javascript code

--- src/script.js
+++ src/script.js
@@ -1,39 +1,3 @@
-function updateApp(response){
-    let cityElement = document.querySelector("#current-city");
-    cityElement.innerHTML = response.data.city;
-
-    let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = response.data.temperature.humidity;
-
-    let windElement = document.querySelector("#wind");
-    windElement.innerHTML = response.data.wind.speed;
-
-    let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.condition.description;
-    
-    let iconElement = document.querySelector("#icon");
-    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temp-icon"/>`;
-
-    let currentTempElement = document.querySelector("#current-temp");
-    let currentTemp = Math.round(response.data.temperature.current);
-    currentTempElement.innerHTML = currentTemp;
-}
-
-function getApi(city) {
-    let apiKey = '434at24abcb5077obabee921e64ef383';
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-    
-    axios.get(apiUrl).then(updateApp);
-}
-
-function runSearch(event){
-    event.preventDefault();
-    
-    let city = document.querySelector("#search-input").value;
-
-    getApi(city);
-}
-
 function formatDate(date){
     let dayNum = date.getDay();
     let days = ["Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"];
@@ -67,16 +31,50 @@ function formatTime(date){
     return formattedTime;
 }
 
+function updateApp(response){
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = response.data.city;
+
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.temperature.humidity;
+
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = response.data.wind.speed;
+
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.condition.description;
+    
+    let currentDate = new Date(response.data.time * 1000);
+    let currentDateElement = document.querySelector("#current-date");
+    currentDateElement.innerHTML = formatDate(currentDate);
+    let currentTimeElement = document.querySelector("#current-time");
+    currentTimeElement.innerHTML = formatTime(currentDate);
+
+    let iconElement = document.querySelector("#icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="temp-icon"/>`;
+
+    let currentTempElement = document.querySelector("#current-temp");
+    let currentTemp = Math.round(response.data.temperature.current);
+    currentTempElement.innerHTML = currentTemp;
+}
+
+function getApi(city) {
+    let apiKey = '434at24abcb5077obabee921e64ef383';
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    
+    axios.get(apiUrl).then(updateApp);
+}
+
+function runSearch(event){
+    event.preventDefault();
+    
+    let city = document.querySelector("#search-input").value;
+
+    getApi(city);
+}
+
 getApi("Durban");
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", runSearch);
-
-let currentDate = new Date();
-
-let currentDateElement = document.querySelector("#current-date");
-currentDateElement.innerHTML = formatDate(currentDate);
-
-let currentTimeElement = document.querySelector("#current-time");
-currentTimeElement.innerHTML = formatTime(currentDate);
 
