6aa7daa5ff5f5ef2be7b88ae1fa2e95d009feab0
kontinew <cybermoon21k@gmail.com>
2025-05-23 02:48:02 +0200

    Coded currentDate and currentTime

--- src/script.js
+++ src/script.js
@@ -24,5 +24,46 @@ function runSearch(response){
     currentTempElement.innerHTML = currentTemp;
 }
 
+function formatDate(date){
+    let dayNum = date.getDay();
+    let days = ["Sunday", "Monday", "Tuesday", "Thursday", "Friday", "Saturday"];
+    let day = days[dayNum];
+
+    let curDate = date.getDate();
+
+    let monthNum = date.getMonth();
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let month = months[monthNum];
+
+    let year = date.getFullYear();
+
+    let formattedDate = `${day}, ${curDate} ${month} ${year}`;
+    return formattedDate;
+}
+
+function formatTime(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+
+    let formattedTime = `${hours}:${minutes}`;
+    return formattedTime;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", getApi);
+
+let currentDate = new Date();
+
+let currentDateElement = document.querySelector("#current-date");
+currentDateElement.innerHTML = formatDate(currentDate);
+
+let currentTimeElement = document.querySelector("#current-time");
+currentTimeElement.innerHTML = formatTime(currentDate);
